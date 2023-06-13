896712fc46667e0d08274fa10390664dbbde5a02
iijima <masashiiijima@gmail.com>
2023-06-13 19:27:42 +0900

    出現位置をランダムに

--- Assets/0_Scripts/GameManager.cs
+++ Assets/0_Scripts/GameManager.cs
@@ -25,8 +25,9 @@ public class GameManager : MonoBehaviour
         //経過時間が繰り返す間隔を経過したら
         if (_timeElapsed >= _repeatSpan)
         {
+            float rnd = Random.Range(-2.0f,2.0f);
             //ここで処理を実行
-            Instantiate(obj, new Vector3(0.0f, 0.5f, 2.0f), Quaternion.identity);
+            Instantiate(obj, new Vector3(rnd, 0.5f, 5.0f), Quaternion.identity);
 
             _timeElapsed = 0;   //経過時間をリセットする
         }
