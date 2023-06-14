79778be6ee06a8650cde407a38a42f1752fd2ae4
Bagakon <SplaBurgerCombo@gmail.com>
2023-06-14 22:45:47 +0900

    曲追加&コルーチン削除

--- Assets/0_Scripts/BPMSoundPlayer.cs
+++ Assets/0_Scripts/BPMSoundPlayer.cs
@@ -8,22 +8,24 @@ public class BPMSoundPlayer : MonoBehaviour
     public AudioClip clip;
 
     public float bpm;
-    private float beatInterval;
+    private float beat_interval;
+    private float beat_timer;
 
     private void Start()
     {
-        if (bpm > 0){
-            beatInterval = 60.0f / bpm; // •b’PˆÊ•ÏŠ·
-            StartCoroutine(PlayBeat()); // ƒRƒ‹[ƒ`ƒ“ŽÀs
+        beat_interval = 60.0f / bpm; // •b’PˆÊ•ÏŠ·
+        beat_timer = 0.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        beat_timer += Time.fixedDeltaTime;
+
+        if (beat_timer >= beat_interval)
+        {
+            beat_timer -= beat_interval;
+            audioSource.PlayOneShot(clip);
         }
     }
 
-    private IEnumerator PlayBeat()
-    {
-        while (true)
-        {
-            audioSource.PlayOneShot(clip);
-            yield return new WaitForSeconds(beatInterval); // beatInterval•b‘Ò‚Â
-        }
-    }
 }
