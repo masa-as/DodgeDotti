5eb74bdded1c599d333b2a9c78bdd0daa684123c
iijima <masashiiijima@gmail.com>
2023-06-14 16:16:49 +0900

    敵と当たった位置が左か右か判定してコントローラーを振動させる

--- Assets/0_Scripts/CheckPlayerHit.cs
+++ Assets/0_Scripts/CheckPlayerHit.cs
@@ -13,9 +13,29 @@ public class CheckPlayerHit : MonoBehaviour
 		img.color = Color.clear;
     }
 
+    public static IEnumerator Vibrate(float duration = 0.1f, float frequency = 0.1f, float amplitude = 0.1f, OVRInput.Controller controller = OVRInput.Controller.Active) {
+        Debug.Log("test");
+        //コントローラーを振動させる
+        OVRInput.SetControllerVibration(frequency, amplitude, controller);
+
+        //指定された時間待つ
+        yield return new WaitForSeconds(duration);
+
+        //コントローラーの振動を止める
+        OVRInput.SetControllerVibration(0, 0, controller);
+    }
+
     // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
+        float player_x = transform.parent.gameObject.transform.position.x;
+        float enemy_x = other.gameObject.transform.position.x;
+        if(enemy_x<player_x){
+            StartCoroutine(Vibrate(duration:0.5f, controller:OVRInput.Controller.LTouch));
+        }
+        if(enemy_x>=player_x){
+            StartCoroutine(Vibrate(duration:0.5f, controller:OVRInput.Controller.RTouch));
+        }
         this.img.color = new Color (0.8f, 0f, 0f, 0.5f);
         Debug.Log("hit");
         Debug.Log(other.gameObject.name);
