000b8b50bb2e70d29e50ae6c684141c3080af58b
iijima <masashiiijima@gmail.com>
2023-06-16 15:08:42 +0900

    Perfectとgoodでエフェクトが出るように

--- Assets/0_Scripts/GoodEffects.cs
+++ Assets/0_Scripts/GoodEffects.cs
@@ -2,20 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PerfectPerticle : MonoBehaviour
+public class GoodEffects : MonoBehaviour
 {
     GameObject obj;
     void Start()
     {
-        obj = (GameObject)Resources.Load("SmallStars");
+        obj = (GameObject)Resources.Load("GoodEffects");
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("colisiontest");
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("colisiontest2");
             GameObject particle = Instantiate(obj, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z - 0.5f), Quaternion.identity);
 
             particle.transform.parent = this.transform;
