1c0964c6e6734b5b15093f1c952c82dfd5f1b61a
Hizawa <hizawarika@gmail.com>
2023-06-16 18:03:17 +0900

    Triggerを取得するScriptを作成

--- Assets/hizawa/Scripts/GlitchSetActive.cs
+++ Assets/hizawa/Scripts/GlitchSetActive.cs
@@ -2,34 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlitchEffects: MonoBehaviour
+public class GlitchEffects : MonoBehaviour
 {
     //public GameObject beam;
-    OVRGrabbable grabbable;
+    //OVRGrabbable grabbable;
 
 
     //Start関数より最初に実行される関数。
     //コンポーネントの取得処理でよく使います
-    private void Awake()
-    {
-        grabbable = GetComponent<OVRGrabbable>();
-    }
+    //private void Awake()
+    //{
+    //    grabbable = GetComponent<OVRGrabbable>();
+    //}
 
     //フレーム毎に実行
     void Update()
     {
-        Glitch();
-    }
 
-    void Glitch()
-    {
-        //右コントローラのトリガーボタン、もしくは左コントローラーのトリガーボタンが押されている間
-        if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger) || OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
-        {
-            GetComponent<ShaderEffect_CorruptedVram>().enabled = true;
+        //void Glitch()
+        //{
+            //右コントローラのトリガーボタン、もしくは左コントローラーのトリガーボタンが押されている間
+            if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger) || OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+            {
+            Debug.Log("trigger");
+            //GetComponent<ShaderEffect_CorruptedVram>().enabled = true;
         }
-        }
+        //}
     }
-
+}
 
 
