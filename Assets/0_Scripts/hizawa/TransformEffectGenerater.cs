84feeaabe416245074f532f2fd3d2d43890e5d3d
Hizawa <hizawarika@gmail.com>
2023-06-18 01:04:47 +0900

    フォルダ整理

--- Assets/0_Scripts/hizawa/TransformEffectGenerater.cs
+++ Assets/0_Scripts/hizawa/TransformEffectGenerater.cs
@@ -11,18 +11,19 @@ public class TransformEffect : MonoBehaviour
     {
 
     }
-    public GameObject _particlePref1;
     // 生成したエフェクト保持
+    public GameObject _particlePref1;
     private GameObject _particleObj1;
 
 
     // Update is called once per frame
     void Update()
     {
+        //E押したら
         if (Input.GetKeyDown(KeyCode.E))
         {
-            _particleObj1 = Instantiate(_particlePref1);
             // エフェクトの位置を自分と同じ位置にする
+            _particleObj1 = Instantiate(_particlePref1);
             _particleObj1.transform.position = transform.position;
 
             
