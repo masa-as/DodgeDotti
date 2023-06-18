c6be9332426f499f4a026e2657fb330c20770a5e
iijima <masashiiijima@gmail.com>
2023-06-18 21:40:40 +0900

    モブを生成するようにした

--- Assets/0_Scripts/SlideMob.cs
+++ Assets/0_Scripts/SlideMob.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlideMusicBox : MonoBehaviour
+public class SlideMob : MonoBehaviour
 {
-    [SerializeField] public float speed = 0.08f;
+    [SerializeField] public float speed = 0.04f;
 
     // Update is called once per frame
     void FixedUpdate()
